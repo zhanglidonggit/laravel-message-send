9ce59da365abd570b50b3dd6e2b9e448b388188d
张立冬 <690191181@qq.com>
2024-11-15 15:49:06 +0800

    更新ReadMe文件

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # laravel-message-send
-send message
+## 安装方法
+```
+    composer require zhanglidong/laravel-message-send
+```
+
+## 配置
+```
+    在config/app.php  providers 项中添加服务提供者
+    MessageNotification\ServiceProvider\NoticeServiceProvider::class
+
+    运行 php artisan vendor:publish 选择上面的提供者或者名称为notice的tag 同步配置文件到config/notice.php
+```
--- src/Config/notice.php
+++ src/Config/notice.php
@@ -4,9 +4,9 @@ return [
     'default' => 'WeChat',
     'channels' => [
         'WeChat' => [
-            'corpid' => 'wx0e69a1a43fea50ed',
-            'corpsecret' => 'o31IMUgjJIHFpniJvyMcQc6vU6uLJA3Y_YkxdoRiZQw',
-            'agentid' => '1000035',
+            'corpid' => '**********',
+            'corpsecret' => '**********',
+            'agentid' => '**********',
             'token_url' => 'https://qyapi.weixin.qq.com/cgi-bin/gettoken',
             'send_url' => 'https://qyapi.weixin.qq.com/cgi-bin/message/send',
             'other_params' => [
@@ -14,9 +14,9 @@ return [
             ],
         ],
         'Dingding' => [
-            'corpid' => 'dinggxzsyw98ywspyqew',
-            'corpsecret' => '6EPJ3GTn3Y1FVERTWuns74CRKfASErZQRN0Pir4bhZK1EhWPmNmPnpm9XLDrJZlU',
-            'agentid' => '2659579676',
+            'corpid' => '**********',
+            'corpsecret' => '**********',
+            'agentid' => '**********',
             'token_url' => 'https://api.dingtalk.com/v1.0/oauth2/accessToken',
             'send_url' => 'https://api.dingtalk.com/v1.0/robot/oToMessages/batchSend',
             'other_params' => [
@@ -26,9 +26,9 @@ return [
             ],
         ],
         'Feishu' => [
-            'corpid' => 'cli_a7b9af88032a100c',
-            'corpsecret' => 'eaVWHnjVHr4woPXcyShFKbJBx4Qa2SCS',
-            'agentid' => '0',
+            'corpid' => '**********',
+            'corpsecret' => '**********',
+            'agentid' => '**********',
             'token_url' => 'https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal',
             'send_url' => 'https://open.feishu.cn/open-apis/message/v4/batch_send/',
             'other_params' => [
